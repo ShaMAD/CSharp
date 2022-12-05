16a5b65ff5b5888d17bd9737a942d7f43df422d6
ShaMAD <at.shamad@gmail.com>
2022-12-05 18:09:34 +0300

    Task 1.8

---
+++ Zad1.8/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 0;
+
+Console.Write("Введмте число: "); int.TryParse(Console.ReadLine()!, out a);
+for (int i = 2; i <= a; i = i + 2)
+{
+    if (i > 2) Console.Write(", ");
+    Console.Write(i);
+}
