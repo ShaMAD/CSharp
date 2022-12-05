ff780b99895155b9d5a8a3731c08441448375875
ShaMAD <at.shamad@gmail.com>
2022-12-05 16:58:56 +0300

    Task 1.4

---
+++ Zad1.2/Program.cs
@@ -0,0 +1,8 @@
+﻿int a,b;
+
+Console.Write ("Введите число 1: "); int.TryParse(Console.ReadLine()!, out a);
+Console.Write ("Введите число 2: "); int.TryParse(Console.ReadLine()!, out b);
+
+if (a < b) a = b;
+
+Console.WriteLine($"max {a}");
---
+++ Zad1.4/Program.cs
@@ -0,0 +1,27 @@
+﻿//int a, b, c;
+int[] array = new int[3]; //Make array length
+int max = 0;
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите число {i + 1} "); int.TryParse(Console.ReadLine()!, out arr[i]);
+    }
+}
+
+FillArray(array);
+
+// Find max
+for (int i = 0; i < array.Length; i++)
+{
+    if (max < array[i]) max = array[i];
+}
+
+Console.WriteLine($"Максимум for: {max}");
+
+// Easy way
+Array.Sort(array);
+Console.WriteLine($"Максимум sort: {array[array.Length - 1]}");
+
+
